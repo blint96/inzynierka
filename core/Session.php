500a7c05a0a8af85cf51bca2be95837b376ddc61
blint96 <sobiec1996@gmail.com>
2019-11-17 17:34:57 +0100

    add flash session messages

--- core/Session.php
+++ core/Session.php
@@ -4,8 +4,16 @@ namespace Framework\Core;
 
 class Session
 {
+    /**
+     * Flash messages
+     * @var array|mixed
+     */
+    private $flashes = [];
+
     public function __construct() {
         session_start();
+
+        $this->flashes = isset($_SESSION['__flashes']) ? $_SESSION['__flashes'] : [];
     }
 
     public function destroy() {
@@ -18,5 +26,21 @@ class Session
 
     public function __set($name, $value) {
         $_SESSION[$name] = $value;
+    }
+
+    public function setFlash($name, $value) {
+        $this->flashes[$name] = $value;
+        $_SESSION['__flashes'] = $this->flashes;
+    }
+
+    public function getFlash($name) {
+        if(isset($this->flashes[$name])) {
+            $value = $this->flashes[$name];
+            unset($this->flashes[$name]);
+            $_SESSION['__flashes'] = $this->flashes;
+            return $value;
+        }
+
+        return NULL;
     }
 }
